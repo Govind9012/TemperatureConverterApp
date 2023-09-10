aad9439246ce0592f446cd15132e4449c9850c7a
Govind Chaudhary <gs9012406068@gmail.com>
2023-09-11 00:27:28 +0530

    UI designed and written logic of the app in this commit

--- app/src/main/java/com/example/temperatureconverterapp/MainActivity.java
+++ app/src/main/java/com/example/temperatureconverterapp/MainActivity.java
@@ -3,13 +3,34 @@ package com.example.temperatureconverterapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    
+    private EditText getValue;
+    private Button button;
+    private TextView textView3;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        getValue = findViewById(R.id.getValue);
+        textView3 =findViewById(R.id.textView3);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String value = getValue.getText().toString();
+                int degree = Integer.parseInt(value);
+                int F = degree*(9/5)+32 ;
+                textView3.setText("The Temperature in °F : "+F);
+            }
+        });
+
+
     }
 }
